f3beb0c2293b45f5eaf5bb75bd0a47dc434807f4
shlok <shlok.agarwal0604@gmail.com>
2025-09-13 21:55:07 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import sortingAlgorithms from "./sortingAlgorithms/sortingAlgorithms";
+
+function App() {
+ <sortingAlgorithms/>
+}
+
+export default App;
---
+++ src/sortingAlgorithms/sortingAlgorithms.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const sortingAlgorithms = () => {
+  return (
+    <div className='primary'>sortingAlgorithms</div>
+  )
+}
+
+export default sortingAlgorithms
